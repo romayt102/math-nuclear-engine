7a79a352faf039ab45e7b4f13cd3e845d983f7e9
romayt102 <romayt102@gmail.com>
2026-08-19 02:41:58 +0500

    feat: cmd-version
    1-ая версия математического движка. На данный момент он работает в консоли. Проверена работа физики.

---
+++ settings.py
@@ -0,0 +1,22 @@
+kernel_reactivity_koef=0.1
+
+base_temperature = 284.0
+fuel_temp_koef = 0.00002
+void_steam_koef = 0.00005
+
+tick=1
+tick_time = 0.1
+min_power = 0.1
+
+heat_koef = 0.00825
+
+environment_temp =20.0
+turbine_koef = 0.2
+
+start_power = 0.0
+start_temperature = 0.0
+start_height_kernel = 0.5
+start_turbine_power = 0.0
+
+max_temperature = 350.0
+max_power=4000.0
